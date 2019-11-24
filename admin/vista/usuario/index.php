5b4a93dbeb0916ed81acc88e05af7bb597cac890
Christian Rivera <criveral2@est.ups.edu.ec>
2019-11-23 21:21:45 -0500

    Actualizar datos del cliente

--- admin/vista/usuario/index.php
+++ admin/vista/usuario/index.php
@@ -10,12 +10,15 @@
 
     <table style="width:100%">
         <tr>
+            <th>Correo</th>   
+            <th>Contraseña</th> 
             <th>Cedula</th>
             <th>Nombres</th>
             <th>Apellidos</th>
             <th>Dirección</th>
             <th>Telefono</th>
-            <th>Correo</th>
+            <th>Eliminado</th>
+            
            
         </tr>
         <?php
@@ -25,14 +28,21 @@
 
         if ($result->num_rows > 0) {
 
+            echo "Usuarios";
             while ($row = $result->fetch_assoc()) {
+                
                 echo "<tr>";
+                echo " <td>" . $row["usu_correo"] . "</td>";
+                echo " <td>" . $row["usu_contrasenia"] . "</td>";
                 echo " <td>" . $row["usu_cedula"] . "</td>";
-                echo " <td>" . $row['usu_nombres'] . "</td>";
-                echo " <td>" . $row['usu_apellidos'] . "</td>";
+                echo " <td>" . $row['usu_nombre'] . "</td>";
+                echo " <td>" . $row['usu_apellido'] . "</td>";
                 echo " <td>" . $row['usu_direccion'] . "</td>";
                 echo " <td>" . $row['usu_telefono'] . "</td>";
-                echo " <td>" . $row['usu_correo'] . "</td>";
+                echo " <td>" . $row["usu_eliminado"] . "</td>";
+                echo " <td> <a href='eliminar.php?codigo=" . $row['usu_id'] . "'>Eliminar</a> </td>";
+                echo " <td> <a href='modificar.php?codigo=" . $row['usu_id'] . "'>Modificar</a> </td>";
+                echo " <td> <a href='cambiar_contrasena.php?codigo=" . $row['usu_id'] . "'>Cambiarcontraseña</a> </td>";
              
                 echo "</tr>";
             }
@@ -41,9 +51,66 @@
             echo " <td colspan='7'> No existen usuarios registradas en el sistema </td>";
             echo "</tr>";
         }
+
+
+
         $conn->close();
         ?>
     </table>
+
+    <table style="width:100%">
+        <tr>
+            <th>Fecha</th>   
+            <th>Hora</th> 
+            <th>Lugar</th>
+            <th>Latitud</th>
+            <th>Longitud</th>
+            <th>Motivo</th>
+            <th>Observaciones</th>
+           
+            
+           
+        </tr>
+        <?php
+        include '../../../config/conexionBD.php';
+      
+
+        /************************************************************************************************ */
+
+        $sql1 = "SELECT * FROM reunion";
+        $result1 = $conn->query($sql1);
+
+        if ($result1->num_rows > 0) {
+
+            echo "Reuniones";
+            while ($row1 = $result1->fetch_assoc()) {
+                
+                echo "<tr>";
+                echo " <td>" . $row1["reu_fecha"] . "</td>";
+                echo " <td>" . $row1["reu_hora"] . "</td>";
+                echo " <td>" . $row1["reu_lugar"] . "</td>";
+                echo " <td>" . $row1['reu_latitud'] . "</td>";
+                echo " <td>" . $row1['reu_longitud'] . "</td>";
+                echo " <td>" . $row1['reu_motivo'] . "</td>";
+                echo " <td>" . $row1['reu_observaciones'] . "</td>";
+             
+                echo "</tr>";
+            }
+        } else {
+            echo "<tr>";
+            echo " <td colspan='7'> No existen reuniones registradas en el sistema </td>";
+            echo "</tr>";
+        }
+        /******************************************************************************* */
+
+
+
+        $conn->close();
+        ?>
+    </table>
+
+
+    
 </body>
 
 </html>
--- admin/vista/usuario/modificar.php
+++ admin/vista/usuario/modificar.php
@@ -3,15 +3,13 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>Eliminar datos de persona</title>
+    <title>Modificar datos de persona</title>
 </head>
 
 <body>
     <?php
-
     $codigo = $_GET["codigo"];
     $sql = "SELECT * FROM usuario where usu_id=$codigo";
-
     include '../../../config/conexionBD.php';
     $result = $conn->query($sql);
 
@@ -19,37 +17,38 @@
 
         while ($row = $result->fetch_assoc()) {
             ?>
-            <form id="formulario01" method="POST" action="../../controladores/usuario/eliminar.php">
+            <form id="formulario01" method="POST" action="../../controladores/usuario/modificar.php">
+
                 <input type="hidden" id="codigo" name="codigo" value="<?php echo $codigo ?>" />
                 <label for="cedula">Cedula (*)</label>
-                <input type="text" id="cedula" name="cedula" value="<?php echo $row["usu_cedula"]; ?>" disabled />
+                <input type="text" id="cedula" name="cedula" value="<?php echo $row["usu_cedula"]; ?>" required placeholder="Ingrese la cedula ..." />
                 <br>
                 <label for="nombres">Nombres (*)</label>
                 <input type="text" id="nombres" name="nombres" value="<?php echo $row["usu_nombre"];
-                                                                                ?>" disabled />
+                                                                                ?>" required placeholder="Ingrese los dos nombres ..." />
                 <br>
                 <label for="apellidos">Apelidos (*)</label>
                 <input type="text" id="apellidos" name="apellidos" value="<?php echo $row["usu_apellido"];
-                                                                                    ?>" disabled />
+                                                                                    ?>" required placeholder="Ingrese los dos apellidos ..." />
                 <br>
                 <label for="direccion">Dirección (*)</label>
                 <input type="text" id="direccion" name="direccion" value="<?php echo $row["usu_direccion"];
-                                                                                    ?>" disabled />
+                                                                                    ?>" required placeholder="Ingrese la dirección ..." />
                 <br>
                 <label for="telefono">Teléfono (*)</label>
                 <input type="text" id="telefono" name="telefono" value="<?php echo $row["usu_telefono"];
-                                                                                ?>" disabled />
+                                                                                ?>" required placeholder="Ingrese el teléfono ..." />
                 <br>
-              
+           
                 <label for="correo">Correo electrónico (*)</label>
-                <input type="email" id="correo" name="correo" value="<?php echo $row["usu_correo"]; ?>" disabled />
+                <input type="email" id="correo" name="correo" value="<?php echo $row["usu_correo"]; ?>" required placeholder="Ingrese el correo electrónico ..." />
                 <br>
 
                 <label for="correo">Contraseña (*)</label>
-                <input type="text" id="contrasenia" name="contrasenia" value="<?php echo $row["usu_contrasenia"]; ?>" disabled />
+                <input type="text" id="contrasenia" name="contrasenia" value="<?php echo $row["usu_contrasenia"]; ?>" required placeholder="Ingrese la contraseña ..." />
                 <br>
 
-                <input type="submit" id="eliminar" name="eliminar" value="Eliminar" />
+                <input type="submit" id="modificar" name="modificar" value="Modificar" />
                 <input type="reset" id="cancelar" name="cancelar" value="Cancelar" />
             </form>
     <?php
